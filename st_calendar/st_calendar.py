f677f0ef18142e297a261d5fe9107cb1d47446a5
OKAMOTO-ac <sp25284c@st.omu.ac.jp>
2025-06-28 17:52:43 +0900

    Create st_calendar.py
    streamlit_calendar

---
+++ st_calendar/st_calendar.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import streamlit_calendar as st_calendar
+import json
+
+
+
+# event_list = []
+# json_file = open('events.json', 'r')
+# event = json.load(json_file)
+
+# for e in event:
+#     event_list.append(e)
+
+options = {
+    'initialView': 'dayGridMonth',
+    # left/center/rightの3つの領域に表示するものはこの例の順番でなくてもいい
+    'headerToolbar': {
+        # ヘッダーの左側に表示するものを指定
+        # 日付を移動するボタンが表示される。'today'を省略してもいい
+        'left': 'today prev,next',
+        # ヘッダーの中央に表示するものを指定
+        # 'title'は表示されている日付などのこと
+        'center': 'title',
+        # ヘッダーの右側に表示するものを指定
+        # ビュー名をカンマ区切りで列挙して指定するとビューを切り替えるためのボタンが表示される
+        'right': 'dayGridMonth,timeGridWeek,listWeek',
+    },
+    'footerToolbar': {
+        # ヘッダーと同じものをフッターにも配置できる。配置しない場合は省力する
+        # 'center': 'title',
+    },
+    'titleFormat': {
+        # 例えば月の表記を数字を指定できる
+        # 年/月/日の順番にするのはlocaleで設定
+        'year': 'numeric', 'month': '2-digit', 'day': '2-digit'
+    },
+    'buttonText': {
+        # 各ボタンを日本語化してみる
+        'today': '当日',
+        'month': '月ごと',
+        'week': '週ごと',
+        'day': '日ごと',
+        'list': 'リスト'
+    },
+    'locale': 'ja', # 日本語化する
+    'firstDay': '1', # 週の最初を月曜日(1)にする。デフォルトは日曜日(0)
+}
+
+# calendar = st_calendar.calendar(events = event_list, options = options)
+calendar = st_calendar.calendar(options = options)
+st.write(calendar)
